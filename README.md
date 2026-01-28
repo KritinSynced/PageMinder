efc49b281327a8afc42c615d29125dd950bff717
KRITIN PANDA <kritin006@gmail.com>
2026-01-28 12:58:20 +0530

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# PageMinder
-A frontend-based Library Management System built using HTML, CSS, and JavaScript. It allows users to add, view, issue/return, and delete books. Data is stored using browser localStorage, making it a lightweight and easy-to-use mini project for learning web development basics.
---
+++ script.js
@@ -0,0 +1,61 @@
+let books = JSON.parse(localStorage.getItem("books")) || [];
+
+function displayBooks() {
+  let table = document.getElementById("bookList");
+  table.innerHTML = "";
+
+  books.forEach((book, index) => {
+    let row = table.insertRow();
+
+    row.insertCell(0).innerText = book.title;
+    row.insertCell(1).innerText = book.author;
+    row.insertCell(2).innerText = book.status;
+
+    let actionCell = row.insertCell(3);
+
+    let issueBtn = document.createElement("button");
+    issueBtn.innerText = book.status === "Available" ? "Issue" : "Return";
+    issueBtn.onclick = () => toggleStatus(index);
+
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.onclick = () => deleteBook(index);
+
+    actionCell.appendChild(issueBtn);
+    actionCell.appendChild(deleteBtn);
+  });
+}
+
+function addBook() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+
+  if (title === "" || author === "") {
+    alert("Please enter all fields");
+    return;
+  }
+
+  books.push({ title, author, status: "Available" });
+  localStorage.setItem("books", JSON.stringify(books));
+
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+
+  displayBooks();
+}
+
+function deleteBook(index) {
+  books.splice(index, 1);
+  localStorage.setItem("books", JSON.stringify(books));
+  displayBooks();
+}
+
+function toggleStatus(index) {
+  books[index].status =
+    books[index].status === "Available" ? "Issued" : "Available";
+
+  localStorage.setItem("books", JSON.stringify(books));
+  displayBooks();
+}
+
+displayBooks();
